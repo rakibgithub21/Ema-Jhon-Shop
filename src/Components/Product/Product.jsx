5cc424a152fc2d46e3f81cf26006d2c2a0fdd31d
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-10 14:10:30 +0600

    display image name etc in car

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,8 +1,21 @@
 
-const Product = () => {
+const Product = ({ product }) => {
+    console.log(product);
+    const { img, name, price, ratings, seller } = product
     return (
-        <div>
-            
+        <div className="card  bg-base-100 shadow-xl">
+            <figure><img src={img} alt="" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>Price:<span>$</span> {price} </p>
+                <div className="">
+                    <p>Manufacturer: {seller}</p>
+                    <p>Ratings: {ratings} <span>Start</span></p>
+                </div>
+                <div className="bg-[#FFE0B3] rounded-xl">
+                    <button className="w-full p-2 font-semibold">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -8,18 +8,19 @@ const Shop = () => {
         fetch('product.json')
             .then(res => res.json())
             .then(data => setProducts(data))
-    },[])
+    }, [])
     return (
-        <div className="flex">
+        <div className="flex gap-10 my-20">
             <div id="products-container" className="w-4/5">
-                <h1>Total Products : {products.length}</h1>
-                {
-                    products.map(product => <Product
-                        key={product.id}
-                        product={product}
-                    
-                    ></Product>)
-                }
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
+                    {
+                        products.map(product => <Product
+                            key={product.id}
+                            product={product}
+
+                        ></Product>)
+                    }
+                </div>
             </div>
             <div id="cart-container" className="w-1/5">
                 <p>Products cart</p>
